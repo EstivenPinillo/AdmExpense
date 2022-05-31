2e2ebbe20d5549a0711d1d28b861852a7c685923
Estiven Pinillo <stivenpinillocarabali@hotmail.com>
2022-05-30 22:48:54 -0500

    Interfaz del modulo "Venta D." y la navegacion realizadas

--- View/VentaDiaria/VentaDiaria.php
+++ View/VentaDiaria/VentaDiaria.php
@@ -1,5 +1,21 @@
 <?php $this->insert('Components::Head',["title"=>"Venta Diaria"]) ?>
 
 
+<div class="contenedor-form">
+    <form action="" class="form">
+        <h2 class="title-form">Venta D.</h2>
+        <div class="group-input">
+            <label for="FechaIngreso" class="label">Fecha ingreso</label>
+            <input type="date" name="FechaIngreso" id="FechaIngreso" class="input"/>
+        </div>
+        <div class="group-input">
+            <label for="CantidadIngreso" class="label">Cantidad ingreso</label>
+            <input type="text" name="CantidadIngreso" id="CantidadIngreso" class="input" placeholder="Ej: 90.000"/>
+        </div>
+        <div class="group-btn">
+            <button type="submit" class="btn">Agregar</button>
+        </div>
+    </form>
+</div>
 
 <?php $this->insert('Components::Footer') ?>
